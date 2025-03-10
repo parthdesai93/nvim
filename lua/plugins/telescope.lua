d8fb79bf551178716fc6750108f67c255099f261
Parth Desai <psdesai.93@gmail.com>
2025-03-10 12:08:52 -0400

    update telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,9 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find filers' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
 }
